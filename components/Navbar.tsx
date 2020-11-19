4176fbfe5a003cfc68c7a68bc23ed8bfa7483f22
BevilKunG <devilbevilgamer@gmail.com>
2020-11-19 22:25:03 +0700

    init navbar elements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,13 +9,60 @@ const Container = styled.div`
 `
 
 const Logo = styled.img`
+  display: none;
+  width: auto;
+  height: 40px;
+
+  @media (min-width: 768px) {
+    & {
+      display: block;
+    }
+  } 
+`
+
+const LogoMini = styled.img`
+  display: block;
+  width: auto;
+  height: 40px;
+
+  @media (min-width: 768px) {
+    & {
+      display: none;
+    }
+  }
+`
+
+const SearchInput = styled.input`
+
+`
+
+const Filter = styled.img`
+  width: auto;
+  height: 20px;
+
+  @media (min-width: 768px) {
+    & {
+      display: none;
+    }
+  }
 `
 
 const Navbar: FC<any> = ({ filters }) => {
   console.log(filters)
   return (
     <Container>
-      <Logo src={`${process.env.NEXT_PUBLIC_BASE_PATH}/images/halfhalf-logo.png`} />
+      <div>
+        <Logo src={`${process.env.NEXT_PUBLIC_BASE_PATH}/images/halfhalf-logo.png`} />
+        <LogoMini src={`${process.env.NEXT_PUBLIC_BASE_PATH}/images/halfhalf-logo-mini.png`} />
+      </div>
+
+      <div>
+        <SearchInput type="text" placeholder="ค้นหา ชื่อ ร้านอาหาร และเครื่องดื่ม ร้านธงฟ้า ร้านค้า OTOP และสินค้าทั่วไป" />
+      </div>
+
+      <div>
+        <Filter src={`${process.env.NEXT_PUBLIC_BASE_PATH}/images/filter.png`} />
+      </div>
     </Container>
   )
 }
